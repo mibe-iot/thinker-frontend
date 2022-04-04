48337422e99eba28d5720895cb70faf6b14ffae0
Ilya Buglakov <neonlightnight@gmail.com>
2022-04-05 00:08:10 +0300

    feat(modal): add pagination mock

--- src/components/devices/linked/DeviceFullViewModal.js
+++ src/components/devices/linked/DeviceFullViewModal.js
@@ -1,9 +1,11 @@
-import { Box, Button, Divider, Flex, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SimpleGrid, Stack, Text, Textarea, VStack } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Box, Button, Divider, Flex, FormControl, FormLabel, HStack, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SimpleGrid, Spacer, Stack, Text, Textarea, VStack } from "@chakra-ui/react";
 import { DEVICE_DESCRIPTION_LENGTH } from "api/contants";
 import { DeviceActionChips } from "components/devices/linked/DeviceActionChips";
 import { Field, Form, Formik } from "formik";
 import { useSelector } from "react-redux";
 import { useFetchDevicesQuery } from "store/slice/devicesSlice";
+import { useBorderColors } from "styles/theme/foundations/colors";
 import { coalesce } from "utils/utils";
 
 export const DeviceFullViewModal = ({ isOpen, onOpen, onClose }) => {
@@ -45,7 +47,7 @@ export const DeviceFullViewModal = ({ isOpen, onOpen, onClose }) => {
                             <ModalCloseButton />
 
                             <ModalBody pb={6}>
-                                <Stack direction="column">
+                                <Stack direction="column" gap={4}>
                                     <Field
                                         as={Textarea}
                                         id="description"
@@ -55,11 +57,15 @@ export const DeviceFullViewModal = ({ isOpen, onOpen, onClose }) => {
                                         placeholder="Device description"
                                         maxLength={DEVICE_DESCRIPTION_LENGTH}
                                     />
-                                    <Text fontSize="md" fontWeight="semibold">Actions:</Text>
-                                    <DeviceActionChips device={device} />
+                                    <HStack>
+                                        <Text fontSize="md" fontWeight="semibold">Actions:</Text>
+                                        <DeviceActionChips device={device} />
+                                    </HStack>
+
+                                    <Divider />
                                     <SimpleGrid minChildWidth="16rem" gap={6}>
                                         <DeviceModalAdditionalData device={device} />
-
+                                        <DeviceReportsViewer />
                                     </SimpleGrid>
                                 </Stack>
                             </ModalBody>
@@ -81,7 +87,7 @@ const DeviceModalAdditionalData = ({ device }) => {
     console.table(device)
     return (
         <Flex flexDirection="column">
-            <Text fontSize="md" fontWeight="semibold">Device data:</Text>
+            <Text fontSize="md" fontWeight="semibold" mb={2}>Device data:</Text>
             <VStack spacing={4} align="start">
                 <FormControl>
                     <FormLabel htmlFor="address">Address</FormLabel>
@@ -97,5 +103,31 @@ const DeviceModalAdditionalData = ({ device }) => {
                 </FormControl>
             </VStack>
         </Flex>
+    )
+}
+
+const DeviceReportsViewer = () => {
+    return (
+        <VStack align="start" isFullWidth border="1px" borderRadius="lg" px={4}>
+            <HStack width="100%">
+                <Text fontSize="md" fontWeight="semibold" mb={2}>Device reports:</Text>
+                <Spacer />
+                <Pagination />
+            </HStack>
+            <Text>Data</Text>
+        </VStack>
+    )
+}
+
+const Pagination = () => {
+    const pageSize = 10;
+    const itemsCount = 90;
+    const page = 21;
+    return (
+        <HStack isFullWidth alignSelf="center">
+            <IconButton size="lg" borderRadius="full" variant="ghost" icon={<ChevronLeftIcon />} />
+            <Button isActive variant="outline" borderRadius="full" py={2} px={0} border="1px">{page}</Button>
+            <IconButton size="lg" borderRadius="full" variant="ghost" icon={<ChevronRightIcon />} />
+        </HStack>
     )
 }
